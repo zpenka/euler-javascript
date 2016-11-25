3b4255a3c99657e8bb4d3628e8b59d6ced28bfbf
Zack Penka & Ben Kovacevich <zpenka@shutterstock.com>
2016-11-25 17:00:50 -0500

    Don't declare global variable

--- 4.js
+++ 4.js
@@ -1,9 +1,8 @@
 // A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
+const palindromes = [];
 const isPalindrome = (x) => x.toString().split('').reverse().join('') === x.toString();
-
-palindromes = [];
 
 for (let i = 999; i > 100; i--) {
   for (let j = 999; j > 100; j--) {
