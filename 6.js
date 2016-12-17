44817f160d79bf858a248bb17e906da8816a9309
Zack Penka <zpenka@shutterstock.com>
2016-12-17 08:55:46 -0500

    Consistent white space

--- 6.js
+++ 6.js
@@ -7,6 +7,7 @@
 // Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 //
 // Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
 const natural_numbers = [];
 for (let i = 1; i <= 100; i++) {
   natural_numbers.push(i);
